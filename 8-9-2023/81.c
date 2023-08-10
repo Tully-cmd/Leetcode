fee5baea9ff4771d24855e6fce768d89b8c4f65e
Tully-cmd <tullymartin@u.boisestate.edu>
2023-08-09 19:13:16 -0600

    made it faster

--- 8-9-2023/81.c
+++ 8-9-2023/81.c
@@ -1,10 +1,30 @@
-bool search(int* nums, int numsSize, int target){
-  for(int i = 0; i < numsSize; i = i + 1) {
+  bool search(int* nums, int numsSize, int target) {
+    
+    short int i;
 
-    if(nums[i] == target) {
+    for(i = 0; i < numsSize; i = i + 1) {
 
-      return true;
+      if(nums[i] == target) {
+
+        return true;
+      }
+      if(nums[i] > target) {
+
+        break;
+      }
     }
+    
+    for(i = numsSize - 1; i >= 0; i = i - 1) {
+
+      if(nums[i] == target) {
+
+        return true;
+      }
+      if(nums[i] < target) {
+
+        break;
+      }
+    }
+
+    return false;
   }
-  return false;
-}
